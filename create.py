fbcd9c53947f7fc0ebaef8eeec10b16643c17cda
hawkarabdulhaq <111079979+hawkarabdulhaq@users.noreply.github.com>
2024-10-31 09:49:48 +0100

    Create create.py

---
+++ create.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def show_create_dashboard():
+    st.title("Create Dashboard")
+    st.write("This is the placeholder for the Create tab.")
+    st.write("Here, you will be able to create new content, assignments, and manage resources.")
