6cc392a89656e3ef45fcc0394ff6d6ab9bce82c9
hawkarabdulhaq <111079979+hawkarabdulhaq@users.noreply.github.com>
2024-10-31 11:03:14 +0100

    Create enroll.py

---
+++ enroll.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def show_enroll_page():
+    st.title("Enroll Page")
+    st.write("This is the placeholder for the Enroll page.")
+    st.write("Here, you will be able to enroll students in various classes and manage enrollment options.")
